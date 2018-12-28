924a5e35d7b9131e1f12e6994993385731cca146
Christian Arana <coanelmejor@gmail.com>
2018-12-27 20:12:41 -0600

    [2.3] Configurando la API de Lastfm en Vue.js

---
+++ platzimusic/src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+  appName: 'platzimusic',
+  apiKey: 'fd3b75c220515513120d5aaf4e00e8e1',
+  secret: '92e25d91b17f4988e8adc834d5bc5142',
+  registeredTo: 'COANelmejor'
+
+}
+// Here are the details of your new API account.
+// Application name	platzimusic
+// API key	fd3b75c220515513120d5aaf4e00e8e1
+// Shared secret	92e25d91b17f4988e8adc834d5bc5142
+// Registered to	COANelmejor
